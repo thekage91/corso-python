addb771fae1b988501544ec44f74a712f2dcc16d
thekage91 <thekage91@gmail.com>
2024-02-25 16:54:00 +0100

    Creazione prima versione della nostra applicazione in Flask

---
+++ form-di-registrazione.py
@@ -0,0 +1,51 @@
+# app.py
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'una_chiave_segreta_very_secret'
+
+class RegistrationForm(FlaskForm):
+    """
+    Form di registrazione
+    """
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email(message='Email non valida')])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Registrati')
+    
+    
+class LoginForm(FlaskForm):
+    """
+    Form di login
+    """
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Login')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Qui si gestirebbe la registrazione dell'utente (es. salvataggio nel database)
+        flash('Registrazione avvenuta con successo!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Registrazione', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Qui si gestirebbe il login dell'utente
+        flash('Login avvenuto con successo!', 'success')
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
